ad3fabc7ef514e9f198c660f6a439339474dfb40
Ivanegas <paola.martinez2907@gmail.com>
2023-11-27 15:13:21 -0600

    :sparkles: add fonts and delete ids of menu

--- assets/js/data-ref.js
+++ assets/js/data-ref.js
@@ -1,5 +1,44 @@
+/* Funcion para quitar el id del menu en la url */
 document.addEventListener("DOMContentLoaded", function () {
-  let links = document.querySelectorAll("nav ul li a");
+  let links = document.querySelectorAll("nav ul li a"); /* <--- tiene que tener la estructura que tiene el menu en el html */
+
+  links.forEach(function (link) {
+    link.addEventListener("click", function (event) {
+      event.preventDefault();
+
+      let targetRef = link.getAttribute("data-ref");
+
+      let targetSection = document.getElementById(targetRef);
+
+      targetSection.scrollIntoView({
+        behavior: "smooth",
+      });
+    });
+  });
+});
+
+/* Funcion para quitar el id del footer en la url */
+document.addEventListener("DOMContentLoaded", function () {
+  let links = document.querySelectorAll("footer ul li a"); /* <--- tiene que tener la estructura que tiene en el html */
+
+  links.forEach(function (link) {
+    link.addEventListener("click", function (event) {
+      event.preventDefault();
+
+      let targetRef = link.getAttribute("data-ref");
+
+      let targetSection = document.getElementById(targetRef);
+
+      targetSection.scrollIntoView({
+        behavior: "smooth",
+      });
+    });
+  });
+});
+
+/* Funcion para quitar el id del boton en la url */
+document.addEventListener("DOMContentLoaded", function () {
+  let links = document.querySelectorAll("div a"); /* <--- tiene que tener la estructura que tiene en el html */
 
   links.forEach(function (link) {
     link.addEventListener("click", function (event) {
