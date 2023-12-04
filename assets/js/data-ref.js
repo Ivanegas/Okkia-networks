2a6f160ada10e3a718e04fbf20282755286b704c
Ivanegas <paola.martinez2907@gmail.com>
2023-12-04 15:34:35 -0600

    :wrench: fix menu cobertura section

--- assets/js/data-ref.js
+++ assets/js/data-ref.js
@@ -17,6 +17,29 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
+document.addEventListener("DOMContentLoaded", function () {
+  let links = document.querySelectorAll("nav ul li a"); 
+  links.forEach(function (link) {
+    link.addEventListener("click", function (event) {
+      event.preventDefault();
+
+      let targetRefs = link.getAttribute("data-ref").split(' '); // Obtener múltiples referencias
+
+      targetRefs.forEach(function(targetRef) {
+        let targetSection = document.getElementById(targetRef);
+
+        // Verificar si la sección está visible
+        if (targetSection && window.getComputedStyle(targetSection).display !== "none") {
+          targetSection.scrollIntoView({
+            behavior: "smooth",
+          });
+        }
+      });
+    });
+  });
+});
+
+
 /* Funcion para quitar el id del footer en la url */
 document.addEventListener("DOMContentLoaded", function () {
   let links = document.querySelectorAll("footer ul li a"); /* <--- tiene que tener la estructura que tiene en el html */
@@ -57,7 +80,24 @@ document.addEventListener("DOMContentLoaded", function () {
 
 /* Funcion para quitar el id del menu de Master y Retail */
 document.addEventListener("DOMContentLoaded", function(){
-  let links = document.querySelector("section div a");
+  let links = document.querySelectorAll("section div a");
+  links.forEach(function(link){
+    link.addEventListener("click", function(event){
+      event.preventDefault();
+
+      let targetRef = link.getAttribute("data-ref");
+
+      let targetSection = document.getElementById(targetRef);
+
+      targetSection.scrollIntoView({
+        behavior: "smooth",
+      });
+    });
+  });
+});
+
+document.addEventListener("DOMContentLoaded", function(){
+  let links = document.querySelectorAll("a");
   links.forEach(function(link){
     link.addEventListener("click", function(event){
       event.preventDefault();
